fe4d01613efb490019ee3eb2bfe5e349d5cc0823
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-03-16 12:20:00 +0330

    delete first try comments and test

--- lib/mishka_social.ex
+++ lib/mishka_social.ex
@@ -1,18 +1,2 @@
 defmodule MishkaSocial do
-  @moduledoc """
-  Documentation for `MishkaSocial`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MishkaSocial.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/mishka_social_test.exs
+++ test/mishka_social_test.exs
@@ -1,8 +1,4 @@
 defmodule MishkaSocialTest do
   use ExUnit.Case
   doctest MishkaSocial
-
-  test "greets the world" do
-    assert MishkaSocial.hello() == :world
-  end
 end
