a365fe96faeb7a013316bc85790ed699b2fc978b
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-03-16 12:18:35 +0330

    create project with --sup option

---
+++ lib/mishka_social.ex
@@ -0,0 +1,18 @@
+defmodule MishkaSocial do
+  @moduledoc """
+  Documentation for `MishkaSocial`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MishkaSocial.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mishka_social_test.exs
@@ -0,0 +1,8 @@
+defmodule MishkaSocialTest do
+  use ExUnit.Case
+  doctest MishkaSocial
+
+  test "greets the world" do
+    assert MishkaSocial.hello() == :world
+  end
+end
