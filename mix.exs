a4e7861847d40b3d27b92f6851954763405d0c66
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-04-01 13:18:56 +0430

    add information for mix file

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule MishkaSocial.MixProject do
   use Mix.Project
+  @version "0.0.1"
 
   def project do
     [
       app: :mishka_social,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
+      name: "Mishka Social",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      homepage_url: "https://github.com/mishka-group",
+      source_url: "https://github.com/mishka-group/mishka_social",
     ]
   end
 
@@ -26,6 +33,22 @@ defmodule MishkaSocial.MixProject do
       {:ueberauth_github, "~> 0.8.1"},
       {:phoenix, "~> 1.6"},
       {:phoenix_live_view, "~> 0.17.7"}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp description() do
+    "MishkaSocial is a plugin for MishkaCms as a social networking gateway"
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs LICENSE README*),
+      licenses: ["Apache License 2.0"],
+      maintainers: ["Shahryar Tavakkoli"],
+      links: %{"GitHub" => "https://github.com/mishka-group/mishka_social"}
     ]
   end
 end
